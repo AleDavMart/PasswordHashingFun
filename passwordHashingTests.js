3ef8d5d0c089094e3d7eb81e6026e30318858b97
AleDavMart <aleyda.davalos@gmail.com>
2022-05-14 11:55:00 -0700

    new hash created need to solve issues

--- passwordHashingTests.js
+++ passwordHashingTests.js
@@ -4,4 +4,12 @@ const hash = crypto.createHash('md5'); //selecting the hash algorithm that will 
 
 hash.update('password23'); // passing in the password the user created 
 
-console.log('Hashed Password:'+ hash.digest('hex')); // This will return the hashed password
+console.log('MD5 Hashed Password:'+ hash.digest('hex')); // This will return the hashed password
+
+//Will not run due to error in thinking that im updating the same hash
+
+const shaHash = crypto.createHash('ha256'); // selecting the hash 256
+
+shaHash.update('pasword23');
+
+console.log('sha256 Hash password:'+ shaHash.digest('hex'));
